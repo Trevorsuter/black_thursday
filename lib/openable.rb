465e00d9216f7230d2227d0bc6275537052d77d2
Wil McCauley <wmccauley724@gmail.com>
2021-01-12 16:54:55 -0700

    Check for typos

--- lib/openable.rb
+++ lib/openable.rb
@@ -2,6 +2,10 @@ require 'CSV'
 
 module Openable
   def read_from(file)
-    CSV.readlines(file, headers: true, header_converters: :symbol))
+    CSV.read(file, headers: true, header_converters: :symbol)
+  end
+
+  def open_from(file)
+    CSV.open(file, headers: true, header_converters: :symbol)
   end
 end
